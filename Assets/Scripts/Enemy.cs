099b230decbbd217133187c2a48155cf89a0af83
Kévin Rifa <29424183+tomas-ryan-lee@users.noreply.github.com>
2022-01-19 18:06:30 +0100

    KR - Correction du tir

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    [SerializeField] GameObject m_Bullet;
+    [SerializeField] float m_LifePoints = 3;
+
+    private void OnCollisionEnter(Collision m_Bullet) {
+        m_LifePoints--;
+        DestroyEnemy();
+    }
+
+    void DestroyEnemy(){
+        if(m_LifePoints < 1){
+            Destroy(gameObject);
+        }
+    }
+}
