d5d854c86ba904466ee2ed0228cd2719f968d74b
Kévin Rifa <29424183+tomas-ryan-lee@users.noreply.github.com>
2022-01-25 21:46:45 +0100

    KR - Fonction Sauter

--- Assets/Scripts/PlayerMovements.cs
+++ Assets/Scripts/PlayerMovements.cs
@@ -7,20 +7,38 @@ public class PlayerMovements : MonoBehaviour
     [Header("Movement Setup")]
     [SerializeField] float m_TranslationSpeed;
     [SerializeField] float m_RotationSpeed;
-    Rigidbody m_Rigidbody;
+    [SerializeField] float m_jumpForce;
+    public Vector3 m_jump;
+    Rigidbody rigidbodyInstance;
+    public bool grounded = true;
 
-    private void Awake()
+    void Start()
     {
-        m_Rigidbody = GetComponent<Rigidbody>();
+        rigidbodyInstance = GetComponent<Rigidbody>();
     }
-
-    void FixedUpdate()
+ 
+     void FixedUpdate()
     {
         float hInput, vInput;
         hInput = Input.GetAxis("Horizontal");
         vInput = Input.GetAxis("Vertical");
-
+        // Fonction Rotation
         transform.Rotate(Vector3.up * m_RotationSpeed * Time.fixedDeltaTime * hInput);
+        // Fonction Déplacements
         transform.Translate(Vector3.forward * m_TranslationSpeed * Time.fixedDeltaTime * vInput);
+        // Fonction Saut
+        Jump();
+    }
+
+    void Jump(){
+        if(!grounded && transform.position.z < 2) {
+            grounded = true;
+        }
+        // Commande Sauter
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidbodyInstance.velocity = new Vector3(0f, m_jumpForce, 0f);
+            grounded = false;
+        }
     }
 }
