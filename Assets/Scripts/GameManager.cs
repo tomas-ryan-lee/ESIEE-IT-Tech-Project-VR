391c63f867e2cd12986b8a5c98898abff609d7db
Kévin Rifa <29424183+tomas-ryan-lee@users.noreply.github.com>
2022-01-26 23:14:56 +0100

    KR - Intégration de l'UI Score

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("Score Setup")]
+    [SerializeField] public GameObject Score;
+    public int ScoreUI;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,6 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Score.GetComponent<TMPro.TextMeshProUGUI>().text = "Score : " + ScoreUI;
     }
 }
