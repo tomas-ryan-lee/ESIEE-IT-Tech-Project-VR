46c00f2a424dc98ddf6e48ab11b96a9f2b0699bc
Kévin Rifa <29424183+tomas-ryan-lee@users.noreply.github.com>
2022-01-27 16:18:55 +0100

    KR - Mise en place de l'ennemi sur le terrain

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -11,10 +11,25 @@ public class Enemy : MonoBehaviour
     [Header("Score Setup")]
     [SerializeField] public GameObject Score;
     [SerializeField] public int ScoreAdd;
+    [SerializeField] public int SpeedWalk;
     public int ScoreUI;
+    [SerializeField] private bool movingLeft;
 
     private void Start() {
         ScoreUI = 0;
+        movingLeft = true;
+    }
+
+    void Update()
+    {
+        Score.GetComponent<TMPro.TextMeshProUGUI>().text = "Score : " + ScoreUI;
+        if (movingLeft == true) {
+            transform.Translate(Vector3.left * SpeedWalk * Time.deltaTime, Space.World);
+            if (transform.position.x <= -6) movingLeft = false;
+        } else {
+            transform.Translate(Vector3.right * SpeedWalk * Time.deltaTime, Space.World);
+            if (transform.position.x >= 6) movingLeft = true;
+        }
     }
 
     private void OnCollisionExit(Collision m_Bullet) {
@@ -27,10 +42,5 @@ public class Enemy : MonoBehaviour
         if(m_LifePoints < 1){
             Destroy(gameObject);
         }
-    }
-
-    void Update()
-    {
-        Score.GetComponent<TMPro.TextMeshProUGUI>().text = "Score : " + ScoreUI;
     }
 }
