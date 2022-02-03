fdea89dbe86da15610d8fa478aa1606dda101268
Kévin Rifa <29424183+tomas-ryan-lee@users.noreply.github.com>
2022-02-03 22:27:24 +0100

    KR - Mise en place des Scripts

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,125 @@
-namespace STUDENT_NAME{
-    using System.Collections;
-    using System.Collections.Generic;
-    using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SDD.Events;
 
-    public class GameManager : MonoBehaviour
+public enum GameState
+{
+    menu, initLevel, play, pause, gameover, victory
+}
+
+public class GameManager : MonoBehaviour
+{
+    static GameManager m_Instance;
+    public static GameManager Instance { get {
+        return m_Instance; } }
+    GameState m_State;
+    public bool IsPlaying { get { return m_State == GameState.play; } }
+    public bool HasThePlayerWon(int score, int VictoryScore)
     {
+        return score >= VictoryScore;
     }
+
+    int m_Score;
+	[SerializeField] int m_VictoryScore;
+	float m_Countdown;
+	[SerializeField] float m_GameDuration;
+
+    public void SubscribeEvents()
+    {
+        EventManager.Instance.AddListener<PlayButtonClickedEvent>(PlayButtonClicked);
+        EventManager.Instance.AddListener<MainMenuButtonClickedEvent>(MainMenuButtonClicked);
+    }
+
+    public void UnsubscribeEvents()
+    {
+        EventManager.Instance.RemoveListener<PlayButtonClickedEvent>(PlayButtonClicked);
+        EventManager.Instance.RemoveListener<MainMenuButtonClickedEvent>(MainMenuButtonClicked);
+    }
+
+	private void OnEnable()
+	{
+		SubscribeEvents();
+	}
+
+	private void OnDisable()
+	{
+		UnsubscribeEvents();
+	}
+
+	void Start()
+    {
+		SetAndBroadcastScoreAndTime(0, m_GameDuration);
+        Menu();
+    }
+
+    void Update()
+    {
+        if(IsPlaying)
+		{
+			SetAndBroadcastScoreAndTime(m_Score, Mathf.Max(0, m_Countdown - Time.deltaTime));
+			if (m_Countdown == 0)
+			    GameOver();
+		}
+    }
+
+
+    void SetAndBroadcastState(GameState State)
+    {
+        m_State = State;
+		switch (m_State)
+		{
+			case GameState.menu:
+				EventManager.Instance.Raise(new GameMenuEvent());
+				break;
+			case GameState.play:
+				EventManager.Instance.Raise(new GamePlayEvent());
+				break;
+			case GameState.pause:
+				EventManager.Instance.Raise(new GamePauseEvent());
+				break;
+			case GameState.gameover:
+				EventManager.Instance.Raise(new GameOverEvent());
+				break;
+			case GameState.victory:
+				EventManager.Instance.Raise(new GameVictoryEvent());
+				break;
+			default:
+				break;
+		}
+    }
+
+    void SetAndBroadcastScoreAndTime(int score, float time)
+    {
+        m_Score = score;
+		m_Countdown = time;
+		EventManager.Instance.Raise(new GameStatisticsChangedEvent() { eScore = m_Score, eCountdown = m_Countdown });
+    }
+
+    void Menu()
+    {
+        SetAndBroadcastState(GameState.menu);
+    }
+
+	void InitLevel()
+	{
+		SetAndBroadcastState(GameState.initLevel);
+	}
+
+    void GameOver()
+    {
+        SetAndBroadcastState(GameState.gameover);
+    }
+
+
+    // Events' callbacks
+	void PlayButtonClicked(PlayButtonClickedEvent e)
+	{
+		InitLevel();
+	}
+
+	void MainMenuButtonClicked(MainMenuButtonClickedEvent e)
+	{
+		SetAndBroadcastState(GameState.menu);
+	}
 }
---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SDD.Events;
+
+public class MenuManager : MonoBehaviour, IEventHandler
+{
+    [SerializeField] GameObject m_MenuPanel;
+    [SerializeField] GameObject m_PausePanel;
+    [SerializeField] GameObject m_VictoryPanel;
+    [SerializeField] GameObject m_GameOverPanel;
+    List<GameObject> m_AllPanels = new List<GameObject>();
+
+    public void SubscribeEvents()
+    {
+        EventManager.Instance.AddListener<GameMenuEvent>(GameMenu);
+        EventManager.Instance.AddListener<GamePlayEvent>(GamePlay);
+        EventManager.Instance.AddListener<GameOverEvent>(GameOver);
+        EventManager.Instance.AddListener<GameVictoryEvent>(GameVictory);
+    }
+    public void UnsubscribeEvents()
+    {
+        EventManager.Instance.RemoveListener<GameMenuEvent>(GameMenu);
+        EventManager.Instance.RemoveListener<GamePlayEvent>(GamePlay);
+        EventManager.Instance.RemoveListener<GameOverEvent>(GameOver);
+        EventManager.Instance.RemoveListener<GameVictoryEvent>(GameVictory);
+    }
+
+	private void OnEnable()
+	{
+		SubscribeEvents();
+	}
+
+	private void OnDisable()
+	{
+		UnsubscribeEvents();
+	}
+
+    private void Awake() 
+    {
+        m_AllPanels.AddRange(new GameObject[] { m_MenuPanel, m_VictoryPanel, m_GameOverPanel, });
+    }
+
+    void DisplayPanel(GameObject panelGO)
+    {
+        foreach (var item in m_AllPanels) item.SetActive(item == panelGO);
+    }
+
+    void GameMenu(GameMenuEvent e)
+    {
+        DisplayPanel(m_MenuPanel);
+    }
+
+    void GamePlay(GamePlayEvent e)
+    {
+        DisplayPanel(null);
+    }
+
+    void GameOver(GameOverEvent e)
+    {
+        DisplayPanel(m_GameOverPanel);
+    }
+
+    void GameVictory(GameVictoryEvent e)
+    {
+        DisplayPanel(m_VictoryPanel);
+    }
+
+    public void PlayButtonHasBeenClicked()
+    {
+        EventManager.Instance.Raise(new PlayButtonClickedEvent());
+    }
+
+    public void MenuButtonHasBeenClicked()
+    {
+        EventManager.Instance.Raise(new MainMenuButtonClickedEvent());
+    }
+}
