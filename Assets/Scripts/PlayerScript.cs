9edf81e2f97549f412ea99824eac0fdde6ac67a2
Kévin Rifa <29424183+tomas-ryan-lee@users.noreply.github.com>
2022-03-07 22:08:29 +0100

    KR - Frame d'invincibilité

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -4,21 +4,53 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    [Header("Life Points Settings")]
     public int m_Life = 3;
     public GameObject[] m_LifePoints;
 
-    // Update is called once per frame
+    [Header("Invulnerability Settings")]
+    private bool m_isInvincible = false;
+    [SerializeField]
+    public float m_InvincibilityDurationSeconds = 1.5f;
+
     private void Start() {
         Debug.Log(m_LifePoints.Length);
     }
 
-    private void TakeDamage() {
-        m_Life--;
-        Destroy(m_LifePoints[m_Life].gameObject);
-    }
-
-    private void OnCollisionEnter(Collision other) {
+    #region Damages
+    private void OnCollisionEnter(Collision other) 
+    {
         if(other.collider.tag == "EnemyBullet")
             TakeDamage();
     }
+
+    private void TakeDamage() 
+    {
+        if (m_isInvincible) return;
+
+        m_Life--;
+        Destroy(m_LifePoints[m_Life].gameObject);
+
+        StartCoroutine(BecomeTemporarilyInvincible());
+    }
+
+    private IEnumerator BecomeTemporarilyInvincible()
+    {
+        Debug.Log("Player turned invincible!");
+        m_isInvincible = true;
+
+        yield return new WaitForSeconds(m_InvincibilityDurationSeconds);
+
+        m_isInvincible = false;
+        Debug.Log("Player is no longer invincible!");
+    }
+
+    void MethodThatTriggersInvulnerability()
+    {
+        if(!m_isInvincible)
+        {
+            StartCoroutine(BecomeTemporarilyInvincible());
+        }
+    }
+    #endregion
 }
