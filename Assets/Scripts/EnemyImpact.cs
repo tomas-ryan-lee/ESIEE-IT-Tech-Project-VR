f9a82862185ef29b331898c33a51823f3907add9
Kévin Rifa <29424183+tomas-ryan-lee@users.noreply.github.com>
2022-03-08 22:12:53 +0100

    Update EnemyImpact.cs

--- Assets/Scripts/EnemyImpact.cs
+++ Assets/Scripts/EnemyImpact.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 
 public class EnemyImpact : MonoBehaviour
 {
-    /// <summary>
-    /// OnCollisionEnter is called when this collider/rigidbody has begun
-    /// touching another rigidbody/collider.
-    /// </summary>
-    /// <param name="other">The Collision data associated with this collision.</param>
     void OnCollisionEnter(Collision other)
     {
         StartCoroutine(ChangeColorOnHit());
