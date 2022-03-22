7cc779644c4a3dd61ce9dd06052e8276af1935ec
Kévin Rifa <29424183+tomas-ryan-lee@users.noreply.github.com>
2022-03-22 23:20:07 +0100

    KR - Ajout du Setup de la Courbe de Bezier

--- Assets/Scripts/PlayerMovements.cs
+++ Assets/Scripts/PlayerMovements.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class PlayerMovements : MonoBehaviour
 {
+    [Header("Bezier Curve Setup")]
+    [SerializeField] List<Transform> m_CtrlTransforms;
+    [SerializeField] int m_NbPtsOnSpline;
+    [SerializeField] bool m_IsClosed;
+    [SerializeField] float m_PtsDensity;
+
     [Header("Movement Setup")]
     [SerializeField] float m_TranslationSpeed;
     [SerializeField] float m_RotationSpeed;
